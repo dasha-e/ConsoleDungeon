fe31ab1353a68f9110bd38795c82d50677b8b4cd
dasha-e <70523223+dasha-e@users.noreply.github.com>
2022-02-06 22:55:55 +0300

    Update Dungeon.cpp

--- DUNGEON/DUNGEON/Dungeon.cpp
+++ DUNGEON/DUNGEON/Dungeon.cpp
@@ -11,33 +11,33 @@ void Dungeon::SetDungeonColor(int number)
 	switch (number)
 	{
 	case 1:
-		SetConsoleTextAttribute(hStdOut, 14); // желтый
+		SetConsoleTextAttribute(hStdOut, 14); // yellow
 		break;
 	case 2:
-		SetConsoleTextAttribute(hStdOut, 2); //зеленый 
+		SetConsoleTextAttribute(hStdOut, 2); //green 
 		break;
 	case 3:
-		SetConsoleTextAttribute(hStdOut, 4); //красный
+		SetConsoleTextAttribute(hStdOut, 4); //red
 		break;
 	case 4:
-		SetConsoleTextAttribute(hStdOut, 1); // синий
+		SetConsoleTextAttribute(hStdOut, 1); // blue
 		break;
 	case 5:
-		SetConsoleTextAttribute(hStdOut, 6); // коричневый
+		SetConsoleTextAttribute(hStdOut, 6); // brown
 		break;
 	case 6:
-		SetConsoleTextAttribute(hStdOut, 3); // бирюзовый
+		SetConsoleTextAttribute(hStdOut, 3); // cyan
 		break;
 	case 7:
-		SetConsoleTextAttribute(hStdOut, 13); //розовый
+		SetConsoleTextAttribute(hStdOut, 13); // pink
 		break;
 	default:
-		SetConsoleTextAttribute(hStdOut, 15); //белый
+		SetConsoleTextAttribute(hStdOut, 15); // white
 		break;
 	}
 }
 
-void Dungeon::Rotation(float fElapsedTime) //обработка поворота: A - вправо, D - влево
+void Dungeon::Rotation(float fElapsedTime) // rotation processing: A - left, D - right
 {
 	float sign = 0;
 	if (GetAsyncKeyState((unsigned short)'A') & 0x8000) sign = -1.f;
@@ -46,12 +46,12 @@ void Dungeon::Rotation(float fElapsedTime) //обработка поворота: A - вправо, D -
 	fPlayerA += sign * (fSpeed * 0.75f) * fElapsedTime;
 }
 
-bool Dungeon::GetOutOfDungeon()//выход
+bool Dungeon::GetOutOfDungeon() // to get out of dungeon press Q
 {
 	return (GetAsyncKeyState((unsigned short)'Q'));
 }
 
-void Dungeon::Movements(Map map, float fElapsedTime)//обработка движений и столкновений: W- вперед, S - назад. Если столкнулись с стеной - шаг назад.
+void Dungeon::Movements(Map map, float fElapsedTime) // moving and collision processing: W- forward, S - bckward
 {
 	float sign = 0;
 	if (GetAsyncKeyState((unsigned short)'W') & 0x8000) sign = 1.f;
@@ -83,12 +83,12 @@ void Dungeon::DisplayStats(float fElapsedTime, wchar_t *screen)
 bool Dungeon::GoToDungeon(int number)
 {
 	SetDungeonColor(number);
-	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight]; // Массив для записи в буфер
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL); // Буфер экрана
-	SetConsoleActiveScreenBuffer(hConsole); // Настройка консоли
-	DWORD dwBytesWritten = 0; // Для отладки
+	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight]; // buffer array
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL); // ГЃГіГґГҐГ° ГЅГЄГ°Г Г­Г 
+	SetConsoleActiveScreenBuffer(hConsole); // console setup
+	DWORD dwBytesWritten = 0; // debug
 	Map map(number);
-	// Переменные для подсчета пройденного времени
+	// elapsed time count
 	auto tp1 = chrono::system_clock::now();
 	auto tp2 = chrono::system_clock::now();
 
@@ -106,57 +106,57 @@ bool Dungeon::GoToDungeon(int number)
 
 		for (int x = 0; x < nScreenWidth; x++)
 		{
-			float fRayAngle = (fPlayerA - fViewAngle / 2.0f) + ((float)x / (float)nScreenWidth) * fViewAngle; //Направление луча
+			float fRayAngle = (fPlayerA - fViewAngle / 2.0f) + ((float)x / (float)nScreenWidth) * fViewAngle; // ray direction
 
-			float fStepSize = 0.1f;		  // Шаг луча										
-			float fDistanceToWall = 0.0f; //Расстояние до препятствия в направлении fRayAngle
+			float fStepSize = 0.1f;		  // ray step										
+			float fDistanceToWall = 0.0f; // distance to obstacle in direction fRayAngle
 
-			bool bHitWall = false;		// Попадание луча в блок стены
-			bool bBoundary = false;		// Попадание луча между блоками стен
+			bool bHitWall = false;		// ray hits a wall block
+			bool bBoundary = false;		// ray hits between a wall blocks
 
-			float fEyeX = sinf(fRayAngle); // Координата X единичного вектора fRayAngle
-			float fEyeY = cosf(fRayAngle); // Координата Y единичного вектора fRayAngle
+			float fEyeX = sinf(fRayAngle); // X coordinate of the unit vector fRayAngle
+			float fEyeY = cosf(fRayAngle); // Y coordinate of the unit vector fRayAngle
 
-			// Бросаем луч пока он не достигнет стены или не выйдет за границы карты
+			// throwing the ray until it hits the wall or goes out of the map
 			while (!bHitWall && fDistanceToWall < fDepth)
 			{
 				fDistanceToWall += fStepSize;
 				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
 				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
 
-				// Проверяем не вышел ли луч за пределы карты
+				// checking if the ray went outside the map
 				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
 				{
-					bHitWall = true;			//  Если мы вышли за карту, фиксируем соударение на расстоянии видимости
+					bHitWall = true;			// if ray went out of the map, recording a collision at a distance of visibility
 					fDistanceToWall = fDepth;
 				}
 				else
 				{
-					// Луч внутри карты. Проверяем является ли достигнутая точка стеной
+					// ray inside the map, checking if the hitting point is a wall block
 					if (map.field.c_str()[nTestX * nMapWidth + nTestY] == '#')
 					{
-						// Луч достиг стены
+						// ray hitted the wall block
 						bHitWall = true;
 
-						// Отображение ребер
+						// display edges
 						vector<pair<float, float>> p;
 
 						for (int tx = 0; tx < 2; tx++)
 							for (int ty = 0; ty < 2; ty++)
 							{
-								float vy = (float)nTestY + ty - fPlayerY; //Координата Y вектора идущего от наблюдателя к ребру
-								float vx = (float)nTestX + tx - fPlayerX; //Координата X вектора идущего от наблюдателя к ребру
-								float d = sqrt(vx*vx + vy * vy); // Длина вектора идущего от наблюдателя к ребру
-								float dot = (fEyeX * vx / d) + (fEyeY * vy / d); // Скалярное произведение единичных векторов v и fRayAngle
-								p.push_back(make_pair(d, dot)); // Сохраняем результат в массив
+								float vy = (float)nTestY + ty - fPlayerY; //Y coordinate of the vector going from the observer to the edge
+								float vx = (float)nTestX + tx - fPlayerX; //X coordinate of the vector going from the observer to the edge
+								float d = sqrt(vx*vx + vy * vy); // The length of the vector from the observer to the edge
+								float dot = (fEyeX * vx / d) + (fEyeY * vy / d); // Scalar product of unit vectors v and fRayAngle
+								p.push_back(make_pair(d, dot)); // saving result to array
 							}
 
-						// Сортируем ребра от ближнего к дальнему
+						// Sort edges from nearest to farhtest
 						sort(p.begin(), p.end(), [](const pair<float, float> &left, const pair<float, float> &right) 
 						{return left.first < right.first; });
 
-						// Отобразим первые 2 или 3 ребра
-						float fBound = 0.01; // Угол, при котором начинаем различать ребро
+						// display 2 or 3 edges
+						float fBound = 0.01; // angle when we begin to see the edge
 						if (acos(p.at(0).second) < fBound) bBoundary = true;
 						if (acos(p.at(1).second) < fBound) bBoundary = true;
 						if (acos(p.at(2).second) < fBound) bBoundary = true;
@@ -164,33 +164,33 @@ bool Dungeon::GoToDungeon(int number)
 				}
 			}
 
-			// Вычисляем расстояние до пола и потолка
+			// СЃalculating the distance to the floor and ceiling
 			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
 			int nFloor = nScreenHeight - nCeiling;
 
-			// Отображаем стены в зависимости от расстояния до них
+			// Display walls by distance to them
 			short nShade = ' ';
-			if (fDistanceToWall <= fDepth / 4.0f)			nShade = 0x2588;	// Чем ближе, тем светлее	
+			if (fDistanceToWall <= fDepth / 4.0f)			nShade = 0x2588; // nearest	
 			else if (fDistanceToWall < fDepth / 3.0f)		nShade = 0x2593;
 			else if (fDistanceToWall < fDepth / 2.0f)		nShade = 0x2592;
-			else if (fDistanceToWall < fDepth)				nShade = 0x2591;
-			else											nShade = ' ';		// Чем дальше, тем темнее
+			else if (fDistanceToWall < fDepth)			nShade = 0x2591;
+			else							nShade = ' ';	 // farthest
 
-			if (bBoundary)		nShade = '|'; // Выделение ребер
+			if (bBoundary)		nShade = '|'; // edges
 
 			for (int y = 0; y < nScreenHeight; y++)
 			{
-				// Для каждой строки консоли
-				if (y <= nCeiling) // Потолок
+				// For each console line
+				if (y <= nCeiling) // celling
 					screen[y*nScreenWidth + x] = ' ';
 				else if (y > nCeiling && y <= nFloor)
 					screen[y*nScreenWidth + x] = nShade;
-				else // Пол
+				else // floor
 				{
 					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
 					if (b < 0.75)		nShade = '.';
 					else if (b < 0.9)	nShade = '-';
-					else				nShade = ' ';
+					else			nShade = ' ';
 					screen[y*nScreenWidth + x] = nShade;
 				}
 			}
@@ -198,7 +198,7 @@ bool Dungeon::GoToDungeon(int number)
 		DisplayStats(fElapsedTime, screen);
 		DisplayMap(screen, map);
 		screen[nScreenWidth * nScreenHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten); //display dungeoun
 		out = GetOutOfDungeon();
 	}
 	delete[] screen;
